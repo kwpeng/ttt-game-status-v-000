fafa98a64e2db4c65c34305f1f81310c3a0600ec
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-07-10 01:06:56 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -56,8 +56,6 @@ end
 def over?(board)
   if won?(board) == true || draw?(board) == true || full?(board) == true
     return true
-  elsif won?(board) == true || draw?(board) == true || full?(board) == false
-    return true
   else
     return false
   end
