1079dd293b879b4a405d2d5b354f216c34458f2f
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-07-10 00:33:30 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,8 +32,6 @@ WIN_COMBINATIONS.each do |win_combination|
     return win_combination
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
-    end
-  end
     else
    return false
   end
